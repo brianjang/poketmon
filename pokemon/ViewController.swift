536a7826e8fa48ef6396f58fc14af4e2e8a3793e
Brian Jang <brian.jang11@gmail.com>
2016-03-09 23:32:18 +0900

    Initial Commit

---
+++ pokemon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokemon
+//
+//  Created by 장성만 on 2016. 3. 9..
+//  Copyright © 2016년 brian X Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
